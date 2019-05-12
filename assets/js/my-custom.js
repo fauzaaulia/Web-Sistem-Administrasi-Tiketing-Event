557ca0ec1891a09a852b1eca26b21be44298765a
Fauza Aulia <fauza@students.unnes.ac.id>
2019-05-13 05:47:40 +0700

    add info event

--- assets/js/my-custom.js
+++ assets/js/my-custom.js
@@ -1,12 +1,13 @@
 $(function () {
 
-    //Menu
+    //Menu--------------
     $('.addMenuButton').on('click', function (m) {
 
         $('#menuModalLabel').html('Add New Menu');
         $('.modal-footer button[type=submit]').html('Add');
         $('#id').val('');
         $('#menu').val('');
+        $('#priority').val('');
     });
 
     $('.showModalEdit').on('click', function (m) {
@@ -25,13 +26,15 @@ $(function () {
             success: function (data) {
                 $('#id').val(data.id);
                 $('#menu').val(data.menu);
+                $('#priority').val(data.priority);
             }
         });
 
     });
 
-    //Sub-Menu
+    //Sub-Menu--------------
     $('.addSubMenuButton').on('click', function (sm) {
+
         $('#subMenuModalLabel').html('Add New Submenu');
         $('.modal-footer button[type=submit]').html('Add');
         $('#id').val('');
@@ -39,7 +42,7 @@ $(function () {
         $('#menu_id').val('');
         $('#url').val('');
         $('#icon').val('');
-        $('#is_active').val('1');
+        $('#is_active').val(1);
     });
 
     $('.showModalEditSub').on('click', function (sm) {
@@ -67,7 +70,7 @@ $(function () {
 
     });
 
-    //Admin-Role
+    //Admin-Role--------------
     $('.addAdminRoleButton').on('click', function (r) {
         $('#adminRoleModalLabel').html('Add New Role');
         $('.modal-footer button[type=submit]').html('Add');
@@ -96,7 +99,7 @@ $(function () {
 
     });
 
-    //Events
+    //Events--------------
     $('.addEventButton').on('click', function (ev) {
 
         $('#eventModalLabel').html('Add New Event');
@@ -115,19 +118,19 @@ $(function () {
 
         $('#eventModalLabel').html('Edit Events');
         $('.modal-footer button[type=submit]').html('Update');
-        $('.modal-content form').attr('action', 'http://localhost/wpu-login/events/updateevent');
+        $('.modal-content form').attr('action', 'http://localhost/web-sate/events/updateevent');
 
         const id = $(this).data('id');
 
         $.ajax({
-            url: 'http://localhost/wpu-login/events/geteditevent',
+            url: 'http://localhost/web-sate/events/geteditevent',
             data: { id: id },
             method: 'post',
             dataType: 'json',
             success: function (data) {
                 $('#id').val(data.id);
                 $('#user_id').val(data.user_id);
-                $('#name').val(data.name);
+                $('#name').val(data.event);
                 $('#date').val(data.date);
                 $('#location').val(data.location);
                 $('#address').val(data.address);
@@ -135,6 +138,33 @@ $(function () {
                 $('#is_active').val(data.is_active);
             }
         });
+
+    });
+
+    //Info Event--------------
+    $('.infoEventModal').on('click', function (ev) {
+
+        $('#infoEventModalLabel').html('Info Events');
+        $('#name2').html('Namanya');
+
+        // const id = $(this).data('id');
+
+        // $.ajax({
+        //     url: 'http://localhost/web-sate/events/geteditevent',
+        //     data: { id: id },
+        //     method: 'post',
+        //     dataType: 'json',
+        //     success: function (data) {
+        //         $("#id").text(data.id);
+        //         $("#user_id").text(data.user_id);
+        //         $("#name").text(data.name);
+        //         $('#date').text(data.date);
+        //         $('#location').text(data.location);
+        //         $('#address').text(data.address);
+        //         $('#phone').text(data.phone);
+        //         $('#is_active').text(data.is_active);
+        //     }
+        // });
 
     });
 
