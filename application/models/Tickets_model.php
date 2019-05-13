1e56c1d8bbde7eedd7a8ac3e85c97db6568ede21
Fauza Aulia <fauza@students.unnes.ac.id>
2019-05-14 00:05:08 +0700

    menambahkan fungsi update tiket

--- application/models/Tickets_model.php
+++ application/models/Tickets_model.php
@@ -3,8 +3,23 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Tickets_model extends CI_Model
 {
-    public function getTiketById($id)
+    public function getTicketById($id)
     {
-        return $this->db->get_where('ticket', ['id' => $id])->row_array();
+        return $this->db->get_where('tickets', ['id' => $id])->row_array();
+    }
+
+    public function updateTicket()
+    {
+        $data = [
+            "id" => $this->input->post('id', true),
+            "event_id" => $this->input->post('event_id', true),
+            "ticket" => $this->input->post('ticket', true),
+            "price" => $this->input->post('price', true),
+            "total" => $this->input->post('total', true),
+            "is_active" => $this->input->post('is_active', true)
+        ];
+
+        $this->db->where('id', $this->input->post('id'));
+        $this->db->update('tickets', $data);
     }
 }
