6541e77b51474da7c35f72de156af0d708821f79
Fauza Aulia <fauza@students.unnes.ac.id>
2019-05-13 23:54:49 +0700

    Menambahkan member controller

---
+++ application/controllers/Member.php
@@ -0,0 +1,37 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Member extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        is_logged_in();
+    }
+
+    public function index()
+    {
+        $data['title'] = 'Dashboard';
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('member/index', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function list()
+    {
+        $data['title'] = 'Seller';
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+
+        $data['users'] = $this->db->get_where('user',  ['role_id' => 3])->result_array();
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('member/list', $data);
+        $this->load->view('templates/footer');
+    }
+}
