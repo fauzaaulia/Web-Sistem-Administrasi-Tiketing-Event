29cfe555a01f1f10acb8aa6ebf4acbdff1c2e36b
Fauza Aulia <fauza@students.unnes.ac.id>
2019-05-19 14:39:19 +0700

    add alert validation

--- application/views/events/index.php
+++ application/views/events/index.php
@@ -6,7 +6,15 @@
 
     <div class="row">
         <div class="col-lg-12">
+
+            <?php if (validation_errors()) : ?>
+                <div class="alert alert-danger" role="alert">
+                    <?= validation_errors(); ?>
+                </div>
+            <?php endif; ?>
+
             <?= $this->session->flashdata('message'); ?>
+
             <a href="" class="btn btn-primary mb-3 addEventButton" data-toggle="modal" data-target="#eventModal">Add New Event</a>
 
             <!-- DataTales Example -->
@@ -28,12 +36,14 @@
                                 <?php foreach ($event as $ev) : ?>
                                     <tr>
                                         <th scope="row"><?= $i; ?></th>
-                                        <td><?= $ev['name']; ?></td>
+                                        <td><?= $ev['event']; ?></td>
                                         <td><?= $ev['date']; ?></td>
                                         <td><?= $ev['location']; ?></td>
                                         <td>
-                                            <a href="<?= base_url(); ?>events/info/<?= $ev['id']; ?>" class="badge badge-info" data-toggle="modal" data-target="#infoModal" data-id="<?= $ev['id']; ?>">info</a>
+                                            <a href="<?= base_url(); ?>events/info/<?= $ev['id']; ?>" class="badge badge-info infoEventModal" data-toggle="modal" data-target="#infoEventModal" data-id="<?= $ev['id']; ?>">info</a>
+
                                             <a href="<?= base_url(); ?>events/edit/<?= $ev['id']; ?>" class="badge badge-success showModalEditEvent" data-toggle="modal" data-target="#eventModal" data-id="<?= $ev['id']; ?>">edit</a>
+
                                             <a href="<?= base_url(); ?>events/delete/<?= $ev['id']; ?>" class="badge badge-danger" onclick="return confirm('Are you sure you want to delete the event?');">delete</a>
                                         </td>
                                     </tr>
@@ -71,27 +81,22 @@
                     <div class="form-group">
                         <label for="event"> Event name </label>
                         <input type="text" class="form-control" id="name" name="name">
-                        <small class="form-text text-danger"><?= form_error('event'); ?></small>
                     </div>
                     <div class="form-group">
                         <label for="date"> Date </label>
                         <input type="text" name="date" id="date" class="form-control" />
-                        <small class="form-text text-danger"><?= form_error('date'); ?></small>
                     </div>
                     <div class="form-group">
                         <label for="location"> Location </label>
                         <input type="text" class="form-control" id="location" name="location">
-                        <small class="form-text text-danger"><?= form_error('location'); ?></small>
                     </div>
                     <div class="form-group">
                         <label for="address"> Address </label>
                         <input type="text" class="form-control" id="address" name="address">
-                        <small class="form-text text-danger"><?= form_error('address'); ?></small>
                     </div>
                     <div class="form-group">
                         <label for="phone"> Phone </label>
                         <input type="text" class="form-control" id="phone" name="phone">
-                        <small class="form-text text-danger"><?= form_error('phone'); ?></small>
                     </div>
                     <input type="hidden" class="form-control" id="is_active" name="is_active">
                 </div>
@@ -105,17 +110,21 @@
 </div>
 
 <!-- Modal Info -->
-<div class="modal fade" id="infoModal" tabindex="-1" role="dialog" aria-labelledby="infoModalLabel" aria-hidden="true">
+<div class="modal fade" id="infoEventModal" tabindex="-1" role="dialog" aria-labelledby="infoEventModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="infoModalLabel">Modal title</h5>
+            <div class="modal-header ">
+                <h5 class="modal-title" id="infoEventModalLabel">Modal title</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
-                ...
+                <h3 id="name2"><b> Nama </b></h3>
+                <h6 id="date2"> Tanggal </h6>
+                <h6 id="location2"> Lokasi </h6>
+                <h6 id="address2"> Address </h6>
+                <h6 id="phone2"> Phone </h6>
             </div>
         </div>
     </div>
