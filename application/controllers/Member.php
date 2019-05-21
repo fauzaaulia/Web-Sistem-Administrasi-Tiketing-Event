f9ce29281987d88f9de080297926cfd7554a8f61
Fauza Aulia <fauza@students.unnes.ac.id>
2019-05-21 13:05:25 +0700

    member update add seller

--- application/controllers/Member.php
+++ application/controllers/Member.php
@@ -23,15 +23,75 @@ class Member extends CI_Controller
 
     public function list()
     {
-        $data['title'] = 'Seller';
+        $data['title'] = 'List Seller';
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
 
         $data['users'] = $this->db->get_where('user',  ['role_id' => 3])->result_array();
 
-        $this->load->view('templates/header', $data);
-        $this->load->view('templates/sidebar', $data);
-        $this->load->view('templates/topbar', $data);
-        $this->load->view('member/list', $data);
-        $this->load->view('templates/footer');
+        $this->form_validation->set_rules('name', 'Name', 'required|trim');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
+            'is_unique' => 'This email has already registered!'
+        ]);
+        $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]', [
+            'matches' => 'Password dont match!',
+            'min_length' => 'Password too short!'
+        ]);
+        $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
+
+        if ($this->form_validation->run() == false) {
+            $this->load->view('templates/header', $data);
+            $this->load->view('templates/sidebar', $data);
+            $this->load->view('templates/topbar', $data);
+            $this->load->view('member/list', $data);
+            $this->load->view('templates/footer');
+        } else {
+            $email = $this->input->post('email', true);
+            $phone = $this->input->post('phone', true);
+            $data = [
+                'name' => htmlspecialchars($this->input->post('name', true)),
+                'email' => htmlspecialchars($email),
+                'phone' => htmlspecialchars($phone),
+                'image' => 'default.jpg',
+                'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
+                'role_id' => 3,
+                'is_active' => 1,
+                'date_created' => time()
+            ];
+
+            $this->db->insert('user', $data);
+
+            $this->_sendEmail('activation');
+
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Congratulation! your account has been created. Please activate your account</div>');
+            redirect('member/list');
+        }
+    }
+
+    private function _sendEmail($type)
+    {
+        $config = [
+            'protocol'  => 'smtp',
+            'smtp_host' => 'ssl://smtp.googlemail.com',
+            'smtp_user' => 'sistem.sate@gmail.com',
+            'smtp_pass' => 'a`1234567890-=',
+            'smtp_port' => 465,
+            'mailtype'  => 'html',
+            'charset'   => 'utf-8',
+            'newline'   => "\r\n"
+        ];
+
+        $this->email->initialize($config);
+
+        $this->email->from('sistem.sate@gmail.com', 'S.A.T.E');
+        $this->email->to($this->input->post('email'));
+
+        if ($type == 'activation') {
+            $this->email->subject('Account Verification');
+            $this->email->message('Berikut adalah inforasi akun kamu. <br> email :' . $this->input->post('email') . '<br> password :' . $this->input->post('password2') . '<br> <br>Silahkan login <a href="' . base_url('/auth') . '">disini</a>');
+            $this->email->send();
+        } else {
+            echo $this->email->print_debugger();
+            die;
+        }
     }
 }
