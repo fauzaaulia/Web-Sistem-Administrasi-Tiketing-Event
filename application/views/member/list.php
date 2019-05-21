d4ef821b5683907a762a78647e9d6945b5d2cd8b
Fauza Aulia <fauza@students.unnes.ac.id>
2019-05-21 13:05:08 +0700

    update add seller

--- application/views/member/list.php
+++ application/views/member/list.php
@@ -6,9 +6,15 @@
 
     <div class="row">
         <div class="col-lg-12">
+            <?php if (validation_errors()) : ?>
+                <div class="alert alert-danger" role="alert">
+                    <?= validation_errors(); ?>
+                </div>
+            <?php endif; ?>
+
             <?= $this->session->flashdata('message'); ?>
 
-            <a href="" class="btn btn-primary mb-3 addSellerButton" data-toggle="modal" data-target="#menuModal">Add New Seller</a>
+            <a href="" class="btn btn-primary mb-3 addSellerButton" data-toggle="modal" data-target="#sellerModal">Add New Seller</a>
 
             <!-- DataTales Example -->
             <div class="card mb-4">
@@ -21,7 +27,7 @@
                                     <th>Name</th>
                                     <th>Email</th>
                                     <th>Phone</th>
-                                    <th>Active</th>
+                                    <th>Access</th>
                                     <th>Action</th>
                                 </tr>
                             </thead>
@@ -58,3 +64,41 @@
 
 </div>
 <!-- End of Main Content -->
+
+<!-- Modal -->
+<div class="modal fade" id="sellerModal" tabindex="-1" role="dialog" aria-labelledby="sellerModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="sellerModalLabel">Add New Seller</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form action="<?= base_url('member/list'); ?>" method="post">
+                <div class="modal-body">
+                    <input type="hidden" class="form-control" id="id" name="id">
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="name" name="name" placeholder="Seller Name">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="email" name="email" placeholder="email">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="phone" name="phone" placeholder="Number Phone">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control" id="password1" name="password1" placeholder="Password">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control" id="password2" name="password2" placeholder="Repeat Password">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Add</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
