a86589606370ea70d95d01ab78dea53f18024e0d
Fauza Aulia <fauza@students.unnes.ac.id>
2019-07-07 21:50:30 +0700

    fixed connect dashboard to database

--- application/views/member/index.php
+++ application/views/member/index.php
@@ -14,7 +14,7 @@
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Total Event</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">11</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $totev["count(event)"]; ?></div>
                         </div>
                         <div class="col-auto">
                             <i class="fas fa-user fa-2x text-gray-300"></i>
@@ -48,7 +48,7 @@
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Jumlah Seller</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">22</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $tosel[0]["count(name)"]; ?></div>
                         </div>
                         <div class="col-auto">
                             <i class="fas fa-users fa-2x text-gray-300"></i>
