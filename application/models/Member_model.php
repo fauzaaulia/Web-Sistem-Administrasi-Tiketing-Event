ae76514aad470a4cf44ecfa1e6f55be44beb96ba
Fauza Aulia <fauza@students.unnes.ac.id>
2019-07-11 20:46:57 +0700

    add fun getToralEventbyUser

--- application/models/Member_model.php
+++ application/models/Member_model.php
@@ -1,13 +1,22 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Admin_model extends CI_Model
+class Member_model extends CI_Model
 {
     public function getTotalSeller()
     {
+        $user_id = $this->session->userdata('id');
         $query = "SELECT count(name)
                   FROM user
-                  WHERE parent_id=12";
+                  WHERE parent_id = $user_id";
         return $this->db->query($query)->result_array();
+    }
+
+    public function getTotalEventByUser()
+    {
+        $user_id = $this->session->userdata('id');
+        $query = "SELECT count(event)
+                  FROM events WHERE user_id = $user_id";
+        return $this->db->query($query)->row_array();
     }
 }
